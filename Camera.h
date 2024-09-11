887495fe1cf264f27b0872c5f1f103e7b67f2114
oliwilliams1 <imhungryoliver@gmail.com>
2024-09-11 18:30:23 +1200

    Perlin noise terrain generation

--- Camera.h
+++ Camera.h
@@ -11,8 +11,8 @@ private:
     double* deltaTime;
     float speed = 5.0f;
 
-    glm::vec3 position = glm::vec3(5.0f, 5.0f, 5.0f);
-    glm::vec3 viewDir = glm::vec3(-1.0f, -1.0f, -1.0f);
+    glm::vec3 position = glm::vec3(5.0f, 3.0f, 5.0f);
+    glm::vec3 viewDir = glm::vec3(-1.0f, -0.6f, -1.0f);
     glm::vec3 target = position + viewDir;
     glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
 
